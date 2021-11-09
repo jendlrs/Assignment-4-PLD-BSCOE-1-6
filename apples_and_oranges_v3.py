d28483634e59db30fa20974d9b462e4aa376cf6d
jendlrs <jensennics.dlrs@gmail.com>
2021-11-09 14:20:20 +0800

    added cost and formula

--- apples_and_oranges_v3.py
+++ apples_and_oranges_v3.py
@@ -7,9 +7,17 @@ def getApplesAndOranges():
     quantity_appleA= int(input("\nHow many apple do you want to buy?\n> "))
     quantity_orangeA= int(input("\nHow many orange do you want to buy?\n> "))
     return quantity_appleA, quantity_orangeA
+
+def computeForTotalCost():
+    totalA= 20*quantity_apple + 25*quantity_orange
+    return totalA
+
 #steps
 #ask how many apples and oranges the user wants to buy.
-quantity_apple, quantity_orange= getApplesAndOranges()
-#add the fix price
-#add formula for total
+
+quantity_apple, quantity_orange =getApplesAndOranges()
+
+#add the fix price and formula
+
+total= computeForTotalCost()
 #display total
