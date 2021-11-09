bbbdb658493b44981b6f6aa7da593222753f3965
jendlrs <jensennics.dlrs@gmail.com>
2021-11-09 15:34:43 +0800

    removed some lines

--- apples_and_oranges_v3.py
+++ apples_and_oranges_v3.py
@@ -1,21 +1,13 @@
+print ("\nWelcome to Jensen's fruit stand!\n The price per apple is 20 PHP\n The price per orange is 25 PHP")
+
 def getApplesAndOranges():
-    quantity_appleA= int(input("\nHow many apple do you want to buy?\n> "))
-    quantity_orangeA= int(input("\nHow many orange do you want to buy?\n> "))
+    quantity_appleA= int(input("\nHow many apple(s) do you want to buy?\n> "))
+    quantity_orangeA= int(input("\nHow many orange(s) do you want to buy?\n> "))
     return quantity_appleA, quantity_orangeA
 
-def computeForTotalCost():
-    totalA= 20*quantity_apple + 25*quantity_orange
-    return totalA
+def computeForTotalCost(quantity_AppleB, quantity_OrangeB):
+    totalA= 20*quantity_AppleB + 25*quantity_OrangeB
+    print (f"\nThe total amount is {totalA:,.0f} PHP.\n")
 
-def display(totalA):
-    print (f"\nThe total amount is {totalA:,.0f}\n")
-#steps
-#ask how many apples and oranges the user wants to buy.
-
-quantity_apple, quantity_orange =getApplesAndOranges()
-
-#add the fix price and formula
-
-total= computeForTotalCost()
-#display total
-display(total)
+quantity_apple, quantity_orange = getApplesAndOranges()
+total=computeForTotalCost(quantity_apple, quantity_orange)
