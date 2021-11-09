4d05ef643d2836633e5f711f4d98385e566ed99d
jendlrs <jensennics.dlrs@gmail.com>
2021-11-09 11:19:30 +0800

    added display function

--- name_age_address_v3.py
+++ name_age_address_v3.py
@@ -3,11 +3,14 @@
 #Hi, my name is _____. I am ____ years old and I live in _____ .
 def getNameAgeAddress():
     nameA= input ("\nWhat is your name? \n> ")
-    ageA= input ("\nHow old are you? \n> ")
+    ageA= int(input("\nHow old are you? \n> "))
     addressA= input ("\nWhere do you live? \n> ")
     return nameA, ageA, addressA
 
+def display(nameB, ageB, addressB):
+    print(f"\nHi, my name is {nameB}. I am {ageB} years old and I live in {addressB} \n")
 #steps
 #ask for name, age, and address
 name, age, address =getNameAgeAddress()
 #display
+display(name, age, address)
