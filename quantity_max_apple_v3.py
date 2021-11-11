1bbab0e0e88fba39cf9921b19e1339df4ee0e3bf
jendlrs <jensennics.dlrs@gmail.com>
2021-11-11 11:57:36 +0800

    added else for short of money

--- quantity_max_apple_v3.py
+++ quantity_max_apple_v3.py
@@ -13,14 +13,17 @@ def getMoney_andCostPerApple():
 def max_apple_and_change (moneyA, cost_per_appleA):
     max_appleA = int(moneyA/cost_per_appleA)
     changeA = moneyA % cost_per_appleA
-    return max_appleA, changeA
+    short_moneyA = abs(money-cost_per_apple)
+    return max_appleA, changeA, short_moneyA
 
-def display(max_appleB, changeB):
+def display(max_appleB, changeB, short_moneyB):
     if max_appleB > 1:
-        print (f"\nYou can buy {max_appleB:,} apples and your change is {changeB:,.2f} pesos\n")
+        print (f"\nYou can buy {max_appleB:,} apples and your change is {changeB:,.2f} pesos.\n")
     elif max_appleB == 1:
-        print (f"\nYou can buy {max_appleB:,} apple and your change is {changeB:,.2f} pesos\n")
+        print (f"\nYou can buy {max_appleB:,} apple and your change is {changeB:,.2f} pesos.\n")
+    else:
+        print (f"\nSorry, your money is not enough, you need atleast {short_moneyB:,.2f} to buy an apple\n")
 
 money, cost_per_apple =getMoney_andCostPerApple()
-max_apple, change = max_apple_and_change(money, cost_per_apple)
-display(max_apple, change)
+max_apple, change, short_money = max_apple_and_change(money, cost_per_apple)
+display(max_apple, change, short_money)
