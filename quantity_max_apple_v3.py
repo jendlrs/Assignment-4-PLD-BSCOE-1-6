3645ce8bee80ca0cf95139c2e87ec0aff81e62d9
jendlrs <jensennics.dlrs@gmail.com>
2021-11-11 11:59:35 +0800

    code completed

--- quantity_max_apple_v3.py
+++ quantity_max_apple_v3.py
@@ -1,13 +1,8 @@
-#Create a program which you will enter the amount of money you have, it will also ask for the price of an apple.
-#Display the maximum number of apples that you can buy and the remaining money that you will have.
-#Display the output in the following format.
-#You can buy ___ apples and your change is ___ pesos.
-
 print ("\nWelcome to Jensen's fruit stand!")
 
 def getMoney_andCostPerApple():
     moneyA= float(input("\nEnter the amount of money you have: \n> "))
-    cost_per_appleA=float(input("\nPrice per apple: \n> "))
+    cost_per_appleA= float(input("\nPrice per apple: \n> "))
     return moneyA, cost_per_appleA
 
 def max_apple_and_change (moneyA, cost_per_appleA):
